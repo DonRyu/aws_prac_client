88d777440c7b11f91e9e4fb5f12a776d2af581e2
DonRyu <alpharyu1031@gmail.com>
2023-02-09 18:05:26 -0800

    iniital

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+const API_HOST = ''
 
 function App() {
+  const getImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+
+    axios({
+      baseURL: API_HOST,
+      url: '/images/:username/thumbnail',
+      method: 'POST',
+      data: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [];
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type={"file"} accept={"image/*"} onChange={getImage} />
     </div>
   );
 }
