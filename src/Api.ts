525f7876c5faa5a073e31a8cd71bab1e1531a1e7
DonRyu <alpharyu1031@gmail.com>
2023-02-14 10:19:05 -0800

    initial

---
+++ src/Api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+const API_HOST = process.env.REACT_APP_API_HOST;
+
+class Api {
+  static async requestToServer(path: string, data?: any) {
+    let res = await axios({
+      baseURL: API_HOST,
+      url: `${API_HOST}/${path}`,
+      method: "POST",
+      data: data,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    if (res) {
+      return res.data;
+    }
+  }
+}
+
+export default Api;
--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,53 @@
-import axios from "axios";
-import React from "react";
-const API_HOST = ''
+import React, { useCallback, useEffect, useState } from "react";
+import Api from "./Api";
+const API_HOST = process.env.REACT_APP_API_HOST;
 
 function App() {
-  const getImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) {
-      return;
-    }
-    const formData = new FormData();
-    formData.append("image", e.target.files[0]);
+  const [imgList, setImageList] = useState([]);
 
-    axios({
-      baseURL: API_HOST,
-      url: '/images/:username/thumbnail',
-      method: 'POST',
-      data: formData,
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, [];
+  useEffect(() => {
+    console.log("resrser");
+    Api.requestToServer("getImages", "").then((res: any) => {
+      console.log("imgList=====>", res);
+      setImageList(res);
+    });
+  }, []);
 
-  };
+  const onUploadImage = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (!e.target.files) {
+        return;
+      }
+      const formData = new FormData();
+      formData.append("img", e.target.files[0]);
+      Api.requestToServer("setImage", formData);
+    },
+    []
+  );
 
   return (
-    <div className="App">
-      <input type={"file"} accept={"image/*"} onChange={getImage} />
-    </div>
+    <>
+      <div className="App">
+        <div>
+          <input type={"file"} accept={"image/*"} onChange={onUploadImage} />
+        </div>
+
+        {imgList?.map((item) => {
+          return <ImageBox src={item} />;
+        })}
+      </div>
+    </>
   );
 }
+
+const ImageBox = ({ src }: { src: string }) => {
+  return (
+    <img
+      alt={"img"}
+      src={`${API_HOST}/images/${src}`}
+      style={{ marginTop: 20, objectFit: "contain", width: 300, height: 400 }}
+    />
+  );
+};
 
 export default App;
