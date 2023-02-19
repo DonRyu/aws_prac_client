e3607b1bdd5e8e915e40d1a3123e8dc333cce042
DonRyu <alpharyu1031@gmail.com>
2023-02-18 18:12:33 -0800

    edit script

--- src/Api.ts
+++ src/Api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 const API_HOST = process.env.REACT_APP_API_HOST;
 
-console.log('API_HOST',API_HOST)
-
 class Api {
   static async requestToServer(path: string, data?: any) {
     let res = await axios({
