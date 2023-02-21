c3226905ddc02ffa131cbca1974476690fa153cd
DonRyu <alpharyu1031@gmail.com>
2023-02-21 13:55:15 -0800

    edit ip

--- src/Api.ts
+++ src/Api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_HOST = process.env.NODE_ENV === 'production' ? 'http://100.26.230.30:8080' : 'http://localhost:8080';
+const API_HOST = process.env.NODE_ENV === 'production' ? 'http://54.236.16.200:8080' : 'http://localhost:8080';
 
 class Api {
   static async requestToServer(path: string, data?: any) {
