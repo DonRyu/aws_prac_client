a19a69537f37e38b7ced52a43bd558d6ff77ff18
DonRyu <alpharyu1031@gmail.com>
2023-02-20 16:42:02 -0800

    test3

--- src/Api.ts
+++ src/Api.ts
@@ -4,7 +4,7 @@ const API_HOST = process.env.NODE_ENV === 'production' ? 'http://100.26.230.30:8
 class Api {
   static async requestToServer(path: string, data?: any) {
     let res = await axios({
-      baseURL: `/api`,
+      baseURL: `${API_HOST}/api`,
       url: `${path}`,
       method: "POST",
       data: data,
